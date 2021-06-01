d23c011607ed6e61b6f1753fe41cbec6c0751528
uditmoral <uditmoral@gmail.com>
2021-06-01 12:17:21 +0530

    Initial commit

---
+++ src/main/java/udit/springframework/uditpetclinic/UditPetClinicApplication.java
@@ -0,0 +1,13 @@
+package udit.springframework.uditpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UditPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UditPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/udit/springframework/uditpetclinic/UditPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package udit.springframework.uditpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UditPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
