01a5a35e54f2db29188822938a12c71ec5f688e4
uditmoral <uditmoral@gmail.com>
2021-06-04 19:25:54 +0530

    Add Vet index page and Vet Controller. Closes #3

---
+++ pet-clinic-web/src/main/java/udit/springframework/uditpetclinic/controllers/VetsController.java
@@ -0,0 +1,13 @@
+package udit.springframework.uditpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(){
+        return "vets/index";
+    }
+}
