1bb585da8cf46fc0d3c2004679e62d6a949b5ce9
uditmoral <uditmoral@gmail.com>
2021-06-06 20:34:52 +0530

    listed vets in Vets index page. closes #8

--- pet-clinic-web/src/main/java/udit/springframework/uditpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/udit/springframework/uditpetclinic/controllers/VetsController.java
@@ -1,13 +1,24 @@
 package udit.springframework.uditpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udit.springframework.uditpetclinic.services.VetService;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
+
         return "vets/index";
     }
 }
