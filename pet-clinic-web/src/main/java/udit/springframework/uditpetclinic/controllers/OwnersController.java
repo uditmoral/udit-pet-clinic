b217e22218de77329ff38d2dd2606660092aa06a
uditmoral <uditmoral@gmail.com>
2021-06-06 20:25:23 +0530

    listed owners in Owners index page. closes #9

--- pet-clinic-web/src/main/java/udit/springframework/uditpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/udit/springframework/uditpetclinic/controllers/OwnersController.java
@@ -1,14 +1,26 @@
 package udit.springframework.uditpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udit.springframework.uditpetclinic.services.OwnerService;
 
 @RequestMapping("owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"","/","index","index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
     }
 }
