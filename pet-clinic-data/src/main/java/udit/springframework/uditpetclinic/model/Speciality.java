1cb21633b4baa4397ba1d10277fc10cf61a9b666
uditmoral <uditmoral@gmail.com>
2021-06-12 10:31:14 +0530

    Created Speciality entity and added speciality property to vets. closes #20

---
+++ pet-clinic-data/src/main/java/udit/springframework/uditpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package udit.springframework.uditpetclinic.model;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
