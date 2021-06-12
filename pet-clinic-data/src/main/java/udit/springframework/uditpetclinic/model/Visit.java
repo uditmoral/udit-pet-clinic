47f9fd86c83497178dddc0476ced9f08b3fcfc86
uditmoral <uditmoral@gmail.com>
2021-06-12 10:19:04 +0530

    Created Visit entity and added pet to owners. closes #19

---
+++ pet-clinic-data/src/main/java/udit/springframework/uditpetclinic/model/Visit.java
@@ -0,0 +1,34 @@
+package udit.springframework.uditpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+
+    private LocalDate visitDate;
+    public String description;
+    private Pet pet;
+
+    public LocalDate getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(LocalDate visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
