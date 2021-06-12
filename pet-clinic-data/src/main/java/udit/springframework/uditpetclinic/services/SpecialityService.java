f0e13bdb911c2be0d82a5257b878463a89cb19a2
uditmoral <uditmoral@gmail.com>
2021-06-12 19:16:53 +0530

    Created Speciality map service. closes #24

---
+++ pet-clinic-data/src/main/java/udit/springframework/uditpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package udit.springframework.uditpetclinic.services;
+
+import udit.springframework.uditpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/udit/springframework/uditpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package udit.springframework.uditpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import udit.springframework.uditpetclinic.model.Speciality;
+import udit.springframework.uditpetclinic.services.SpecialityService;
+
+import java.util.Set;
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
